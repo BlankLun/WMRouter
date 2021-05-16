541d0d232994c997b15f82f2971f4c749fc67263
likunlun <sz.lkl@163.com>
2021-05-16 15:30:12 +0800

    添加区分bundle和非bundle时资源引用方式使用示例

--- demo/dynamicfeature/src/main/java/com/lkl/dynamicfeature/DynamicFeatureActivity1.java
+++ demo/dynamicfeature/src/main/java/com/lkl/dynamicfeature/DynamicFeatureActivity1.java
@@ -3,6 +3,7 @@ package com.lkl.dynamicfeature;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.sankuai.waimai.router.annotation.RouterPage;
 import com.sankuai.waimai.router.annotation.RouterUri;
@@ -11,10 +12,15 @@ import com.sankuai.waimai.router.demo.lib2.DemoConstant;
 @RouterPage(path = DemoConstant.TEST_DYNAMIC_FEATURE1, interceptors = TestUriParamInterceptor.class)
 @RouterUri(path = DemoConstant.TEST_DYNAMIC_FEATURE2, interceptors = TestUriParamInterceptor.class)
 public class DynamicFeatureActivity1 extends AppCompatActivity {
+    private TextView mTestBundleResRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dynamic_feature);
+
+        mTestBundleResRef = findViewById(R.id.testBundleResRef);
+
+        mTestBundleResRef.setText(com.sankuai.waimai.router.demo.lib2.R.string.bundle_module_res_ref);
     }
 }
